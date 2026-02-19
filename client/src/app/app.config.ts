7e12e0fc82195cd29dbb9c685eb34247097658c1
BhusariS <surabhibhusari2@gmail.com>
2026-02-19 22:25:20 +0530

    End of section 6

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,14 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { InitService } from '../core/services/init-service';
+import { lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
-    provideHttpClient()
+    provideRouter(routes, withViewTransitions()),
+    provideHttpClient(),
+    provideAppInitializer(async ()=>{
+      const initService =inject(InitService);
+
+      return new Promise<void>((resolve )=>{
+        setTimeout(async ()=>{
+        try{
+        return lastValueFrom(initService.Init())
+        }finally{
+        const spalsh=document.getElementById('initial-splash');
+        if (spalsh){
+          spalsh.remove();
+      }
+      resolve()
+    }
+        },500)
+      })
+      
+    })
   ]
 };
